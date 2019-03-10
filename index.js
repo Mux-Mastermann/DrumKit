e7fedccbebf1ca7c1a529e98a121265a224f043c
Mux-Mastermann <43514294+Mux-Mastermann@users.noreply.github.com>
2019-03-10 21:02:46 +0100

    Added event listener to all buttons

---
+++ index.js
@@ -0,0 +1,9 @@
+for (i=0; i < document.querySelectorAll(".drum").length; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    alert("I got Clicked");
+
+    //What to do when click detected
+  });
+
+}
